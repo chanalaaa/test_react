38af3af3a737a3c3b640434dc83003ef01266d24
Chanala Wilangka <c.wilangka@gmail.com>
2016-06-10 14:55:34 +0700

    endpoint

--- ui/containers/Pages.js
+++ ui/containers/Pages.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 import fetch from 'isomorphic-fetch'
 //Presentational Component from components
 import { Pages } from '../components'
+import { PAGES_ENDPOINT } from '../constants/endpoints'
 
 export default class PagesContainer extends Component {
   state = {
     pages: []
   }
   componentDidMount() {
-    fetch('http://127.0.0.1:5000/api/v1/pages')
+     //fetch('/api/v1/pages') 
+     fetch(PAGES_ENDPOINT)
       .then((response) => response.json())
       .then((pages) => this.setState({ pages }))
   }
