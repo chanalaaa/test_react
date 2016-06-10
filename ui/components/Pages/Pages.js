c3d78e3f36fdfa349eac43d4353261bcb6ee5d6c
Chanala Wilangka <c.wilangka@gmail.com>
2016-06-10 17:39:01 +0700

    Functional Component

--- ui/components/Pages/Pages.js
+++ ui/components/Pages/Pages.js
@@ -2,43 +2,41 @@ import React, { Component,PropTypes } from 'react'
 import fetch from 'isomorphic-fetch'
 import Page from './Page'
 
-export default class Pages extends Component  {
 
-  static propTypes = {
-    pages: PropTypes.array.isRequired,
-    onReloadPages: PropTypes.func.isRequired
-  }
+const Pages = ({pages, onReloadPages}) => (
+  <div className='block'>
+    <button
+      className='button'
+      onClick={() => onReloadPages()}>
+      Reload Pages
+    </button>
+    <hr />
+    <table className='table'>
+    <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          pages.map((page) => (
+            <Page
+              key={page.id}
+              id={page.id}
+              title={page.title} />
+          ))
+        }
+      </tbody>
+    </table>
+  </div>
+)
 
-  render() {
-     const { pages, onReloadPages } = this.props
-    return (
-       <div className='block'>
-        <button 
-          className='button'
-          onClick={() => onReloadPages()}>
-          Reload Pages
-        </button>
-        <hr />
-      <table className='table'>
-      <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            this.props.pages.map((page) => (
-              <Page key={page.id} id={page.id} title={page.title}/> 
-              //<Page key={page.id} page={page} /> 
-            ))
-          }
-        </tbody>
-      </table>
-      </div>
-    )
-  }
+
+Pages.propTypes = {
+  pages: PropTypes.array.isRequired,
+  onReloadPages: PropTypes.func.isRequired
 }
 
 export default Pages
